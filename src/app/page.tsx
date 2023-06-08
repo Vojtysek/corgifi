071b14f88e9228029e4c260863c839a120c9ed3e
Vojtěch Döme <ojta@post.cz>
2023-06-08 14:21:01 +0200

    add: video, scene selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 import { Drag } from "../components/Draggable";
+import Scenes from "@/components/Scenes/Scenes";
 
 export default function Home() {
   return (
     <>
-      <main>
-        <div>
-          <Image
-            className="relative"
-            src="/coffee.jpeg"
-            alt="Picture of the author"
-            fill
-          />
-        </div>
+      <main className="bg-black">
+        <video autoPlay muted loop className="relative opacity-50 h-screen w-screen ">
+          <source src="/monke.mp4" type="video/mp4" />
+        </video>
+
         <div className="absolute top-0 left-0 w-screen h-screen">
+          <Drag>
+            <Scenes />
+          </Drag>
         </div>
       </main>
     </>
